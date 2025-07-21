4ac8cf51657fd5b4a72bc35ed336251487f949b7
Jonathan Carrero <jonathan.carrero.aranda@gmail.com>
2025-07-21 09:49:13 +0200

    hero text change

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -62,7 +62,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Jonathan Carrero</h2>;
-  const three = <h3 className="medium-heading">Optimizando recursos digitales</h3>;
+  const three = <h3 className="medium-heading">Specialized in resource optimization</h3>;
   const four = (
     <>
       <p>
